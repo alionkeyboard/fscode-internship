f2fe09cedefc0f5d871522ab1c9475bdcf55c7ae
kamrankamilli <kamilli818@gmail.com>
2021-02-23 23:31:04 +0400

    added comments(23.02.2021)

--- KamranKamilli/23.02.2021/ImageUpload/controller/fetchimage.php
+++ KamranKamilli/23.02.2021/ImageUpload/controller/fetchimage.php
@@ -1,9 +1,10 @@
 <?php
 
+//select all data from table order by upload time in descending order
 $sql = 'SELECT * FROM images ORDER BY upload_time DESC';
 $stmt = $db->query($sql);
 if ($stmt->num_rows > 0) {
-
+    //if data exist in database the fetch each row
     while ($row = $stmt->fetch_assoc()) {
         $imageURL = 'images/' . $row["image_name"];
 ?>
--- KamranKamilli/23.02.2021/ImageUpload/controller/uploadimage.php
+++ KamranKamilli/23.02.2021/ImageUpload/controller/uploadimage.php
@@ -16,18 +16,20 @@ if($_SERVER['REQUEST_METHOD']==='POST')
         $imagePath = $dir . $imageName;
         $imageType = pathinfo($imagePath,PATHINFO_EXTENSION);
         $arr = array('png','jpg','jpeg','gif');
+        //check if type of the image is acceptable
         if(!in_array($imageType,$arr))
         {
             $message = "Only png, jpg, jpeg, gif format images acceptable";
         }
     }
 
+    //adding images to the system and to the database
     if(empty($message) && move_uploaded_file($_FILES["image"]["tmp_name"], $imagePath)){
 
         $sql = "INSERT INTO images (image_name,image_url) VALUES (?,?)";
 
         if($stmt = $db->prepare($sql)){
-            
+            // bind parameter used to prevent SQL injection
             $stmt->bind_param("ss",$pimageName,$pimagePath);
             $pimageName = $imageName;
             $pimagePath = $imagePath;
