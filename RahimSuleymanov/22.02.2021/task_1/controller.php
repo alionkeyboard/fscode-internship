e272e26e8f0581368ee53969502aeac4e0eb6724
Rahim Süleymanov <rahim.suleymanov94@gmail.com>
2021-02-23 04:11:40 +0400

    task_1

--- RahimSuleymanov/22.02.2021/task_1/controller.php
+++ RahimSuleymanov/22.02.2021/task_1/controller.php
@@ -1,39 +1,46 @@
 <?php
-function myRegexEditor($pattern,$subject,$result = null)
+if(isset($_POST['pattern'],$_POST['string']))
 {
-    preg_match_all($pattern,$subject,$result);
-
-    $output = '';
-    $i = 1;
-    $position = 0;
-    foreach(mb_str_split($subject) as $letter)
+    function myRegexEditor($pattern,$subject,$result = null)
     {
-        if(in_array($letter,$result[0]))
+        preg_match_all($pattern,$subject,$result);
+
+        $output = '';
+        $i = 1;
+        $position = 0;
+        foreach(mb_str_split($subject) as $letter)
         {
-            $title = 'Match '.$i.'<hr/> group 0: '.$letter.'<hr/> pos: '.$position.'-'.($position+1);
-            if($i%2 == 0)
+            if(in_array($letter,$result[0]))
             {
-                $output .= '<a class="tip" style="background-color:#084B8C;">'.$letter.'<span>'.$title.'</span></a>';
+                $title = 'Match '.$i.'<hr/> group 0: '.$letter.'<hr/> pos: '.$position.'-'.($position+1);
+                if($i%2 == 0)
+                {
+                    $output .= '<a class="tip" style="background-color:#084B8C;">'.$letter.'<span>'.$title.'</span></a>';
+                }
+                else
+                {
+                    $output .= '<a class="tip" style="background-color:#05335D;">'.$letter.'<span>'.$title.'</span></a>';
+                }
+                $i++;
             }
             else
             {
-                $output .= '<a class="tip" style="background-color:#05335D;">'.$letter.'<span>'.$title.'</span></a>';
+                $output .= $letter;
             }
-            $i++;
+            $position++;
         }
-        else
-        {
-            $output .= $letter;
-        }
-        $position++;
+        return $output;
     }
-    return $output;
+
+
+    $pattern = $_POST['pattern'];
+
+    $subject = $_POST['string'];
+
+    echo myRegexEditor($pattern,$subject);
 }
-
-
-$pattern = $_POST['pattern'];
-
-$subject = $_POST['string'];
-
-echo myRegexEditor($pattern,$subject);
+else
+{
+    header('location:index.php');
+}
 ?>
