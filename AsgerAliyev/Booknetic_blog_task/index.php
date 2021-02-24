8787d106f9cd1d59037a6462ae03a02b411cc2e4
Askerased <70854483+Askerased@users.noreply.github.com>
2021-02-24 21:46:39 +0400

    desc

--- AsgerAliyev/Booknetic_blog_task/index.php
+++ AsgerAliyev/Booknetic_blog_task/index.php
@@ -4,11 +4,11 @@ $i          = 1;
 $all        = [];
 $page_count = 1;
 
-for ( $page = 1; $page <= $page_count; $page++ )
+for ( $page = 1; $page <= $page_count; $page++ ) // Butun sehifelerde ishlemesi ucun loop
 {
 
 	$url = "https://www.booknetic.com/blog?page=$page";
-	$ch  = curl_init();
+	$ch  = curl_init(); // curl  qurulmasi
 
 	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
 	curl_setopt( $ch, CURLOPT_URL, $url );
@@ -17,13 +17,21 @@ for ( $page = 1; $page <= $page_count; $page++ )
 
 	curl_close( $ch );
 
+	// postlari elde etmek ucun bize lazim olan hisseinin cekilmesi
 	preg_match_all( '@<div class="blog-item">(.*?)</div>@si', $site, $posts );
+
+	// Page sayinin tapilmasi ucun lazim olan hissenin cekilmesi
 	preg_match_all( '@li class="page-item"><a class="page-link" href="https://www\.booknetic\.com/blog\?page=(.*?)">(.*?)</a></li>@si', $site, $pages );
 
-	$page_count = max( $pages[ 1 ] );
-
-	foreach ( $posts[ 0 ] as $post )
+	// burda  esas sehifeden gedilen butun diger sehifelerinin linkini veren array-den en boyuk elementi tapib yuxaridaki loop ucun shert olaraq gonderirik
+	if ( isset( $pages[ 1 ] ) )
 	{
+		$page_count = max( $pages[ 1 ] );
+	}
+
+	foreach ( $posts[ 0 ] as $post ) // Bu loop her page-deki butun postlari o page-in arrayine elave edir
+	{
+		// post melumatlarinin elde edilmesi
 		preg_match_all( '/<h2>(.*?)<\/h2><\/a>/', $post, $title );
 		preg_match_all( '/<span class="bl-date">(.*?)<\/span>/', $post, $date );
 		preg_match_all( '/<p>(.*?)<\/p>/', $post, $description );
@@ -36,15 +44,14 @@ for ( $page = 1; $page <= $page_count; $page++ )
 		$image       = $image[ 1 ][ 0 ];
 		$url         = $url[ 1 ][ 0 ];
 
-		$all[] = [
-			"page_$page" => [
-				"post_$i" => [
-					'title'       => $title,
-					'date'        => $date,
-					'description' => $description,
-					'image'       => $image,
-					'url'         => $url
-				]
+		// melumatlarin array-a daxil edilmesi
+		$all[ "page_$page" ][] = [
+			"post_$i" => [
+				'title'       => $title,
+				'date'        => $date,
+				'description' => $description,
+				'image'       => $image,
+				'url'         => $url
 			]
 		];
 		$i++;
